c40814c6cc25795a3a42f839f2a1c3d50cab82ec
rizwan <rizwan2k10@gmail.com>
2017-12-28 12:44:17 +0530

    rebased

--- test_app1/Program.cs
+++ test_app1/Program.cs
@@ -6,7 +6,16 @@ namespace test_app1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            for (int i=0; i < 20; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
